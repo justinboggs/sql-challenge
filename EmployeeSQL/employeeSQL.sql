611e3f77bddfa54c8029be86ba2498d25ef2d095
Justin Boggs <jboggs1@gmail.com>
2020-02-11 00:23:08 -0600

    updates

--- EmployeeSQL/employeeSQL.sql
+++ EmployeeSQL/employeeSQL.sql
@@ -54,11 +54,33 @@ select * from employees
 select * from salaries
 select * from titles
 
+--Employee number, last name, first name, gender, salary
 select a.emp_no, a.last_name, a.first_name, a.gender, b.salary
 from employees a
 join salaries b
 on (a.emp_no = b.emp_no)
 
+--Employees hired in 1986
 select last_name, first_name, hire_date
 from employees
 where hire_date >= '1986-01-01' AND hire_date <= '1986-12-31'
+
+--Manager of each department
+select c.last_name, c.first_name
+from employees c
+join dept_manager b
+on (c.emp_no = b.emp_no)
+
+select a.dept_no, b.dept_name, a.emp_no, a.from_date, a.to_date
+from dept_manager a
+join departments b
+on (a.dept_no = b.dept_no)
+
+--manager of each department
+select last_name, first_name
+from employees
+where emp_no in
+	(
+	select emp_no
+	from dept_manager
+	)
