5c8cd78eccffb283478f6cc224c15fa81f669cd9
Justin Boggs <jboggs1@gmail.com>
2020-02-14 00:56:55 -0600

    updates

--- EmployeeSQL/employeeDB.sql
+++ EmployeeSQL/employeeDB.sql
@@ -19,6 +19,12 @@ join employees b
 on (a.emp_no = b.emp_no)
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+select a.emp_no, a.last_name, a.first_name, c.dept_name
+from employees a
+join dept_emp b
+on (a.emp_no = b.emp_no)
+join departments c
+on (b.dept_no = c.dept_no)
 
 -- 5. List all employees whose first name is "Hercules" and last names begin with "B."
 select * from employees
@@ -35,5 +41,12 @@ where c.dept_name = 'Sales'
 
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select a.emp_no, a.last_name, a.first_name, c.dept_name
+from employees a
+join dept_emp b
+on (a.emp_no = b.emp_no)
+join departments c
+on (b.dept_no = c.dept_no)
+where c.dept_name in ('Sales', 'Development')
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
